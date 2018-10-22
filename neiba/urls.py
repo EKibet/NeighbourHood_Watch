839a4529e2f4cf6d39ee7be32e3f28e2effeb9b8
EKibet <kibetedgar@gmail.com>
2018-10-22 09:54:45 +0300

    includes urls to neighbourhood

--- neiba/urls.py
+++ neiba/urls.py
@@ -15,20 +15,21 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-from userauth import views as user_views
+from neighbourhood import views as user_views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from userauth import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^',include('neighbourhood.urls')),
+    url(r'^',include('neighbourhood.urls')),
     url(r'^signup$',user_views.register,name='register'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     url(r'profile/$', user_views.profile, name='profile'),
+    url('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout' ),
 
 ]
 if settings.DEBUG:
